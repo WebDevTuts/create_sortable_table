19672a5d9386b50c65ad4871a1e16a3ce8bc2b29
edwinchen85 <edwinchen85@gmail.com>
2017-03-24 21:43:11 +0800

    Create custom sort properties for Date Column

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
+  $.tablesorter.addParser({
+
+    id: 'getDateAttr',
+    is: function(sort) { return false; },
+    format: function(sort, table, cell, cellIndex) { return $(cell).attr('data-date'); },
+    type: 'text'
+
+  });
+
   $('table#courses').tablesorter({
 
     sortList: [[ 2,0 ]],   // sort based on [[ column (zero base), 0:ascending 1:descending ]]
     cssAsc: 'sorted asc',  // css class injection
-    cssDesc: 'sorted desc' // css class injection
+    cssDesc: 'sorted desc', // css class injection
+    headers: { 3 : { sorter: 'getDateAttr' } }
 
   });
 
