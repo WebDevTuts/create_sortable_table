445f19608d8128d05dda4b16b0e3d1e289f6b747
edwinchen85 <edwinchen85@gmail.com>
2017-03-24 21:05:36 +0800

    Set css class injection in plugin setting

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
   $('table#courses').tablesorter({
 
-    sortList: [[ 2,0 ]]   // sort based on [[ column (zero base), 0:ascending 1:descending ]]
+    sortList: [[ 2,0 ]],   // sort based on [[ column (zero base), 0:ascending 1:descending ]]
+    cssAsc: 'sorted asc',  // css class injection
+    cssDesc: 'sorted desc' // css class injection
 
   });
 
